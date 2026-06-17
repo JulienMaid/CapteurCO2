ce68ed7365beb712d0405cda8b027e8f2ff4ec00
Julien B. <jbonne79@yahoo.fr>
2026-06-17 11:17:21 +0200

    Ajout d'un fichier de définition des E/S

---
+++ DefinitionES.h
@@ -0,0 +1,23 @@
+/*
+ * DefinitionES.h
+ *
+ *  Created on: 16 juin 2026
+ *      Author: julien
+ */
+
+#ifndef DEFINITIONES_H_
+#define DEFINITIONES_H_
+
+
+#define ENTREE_BP       0
+
+#define ANALOG_BATTERIE 0
+
+#define CMD_LEDWS       0
+
+
+
+
+
+
+#endif /* DEFINITIONES_H_ */
