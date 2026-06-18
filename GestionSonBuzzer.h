1c8f941173b7175be3de0e4f7d7debd8b2758055
Julien B. <jbonne79@yahoo.fr>
2026-06-18 22:49:13 +0200

    Création d'un classe GestionSonBuzzer dérviée de GestionClignotementLed

---
+++ GestionSonBuzzer.h
@@ -0,0 +1,26 @@
+/*
+ * GestionSonBuzzer.h
+ *
+ *  Created on: 18 juin 2026
+ *      Author: julien
+ */
+
+#ifndef GESTIONSONBUZZER_H_
+#define GESTIONSONBUZZER_H_
+
+#include "GestionClignotementLed.h"
+
+class GestionSonBuzzer: public GestionClignotementLed
+{
+public:
+  GestionSonBuzzer(uint32_t p_u32_PortLed = 0, bool p_b_Reverse = false, bool p_b_AutonomousOperation = true,
+      uint8_t p_u8_SequenceLength = 20);
+  virtual ~GestionSonBuzzer();
+
+  //! Active la séquence n°x
+  virtual void SetSequence(uint8_t p_u8_NumeroSequence) override;
+
+
+};
+
+#endif /* GESTIONSONBUZZER_H_ */
